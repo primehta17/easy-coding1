d30934d0446b913b4d586f7c16e9f1776b563d25
Priyanka Mehta <primehta17@gmail.com>
2025-04-11 19:00:22 +0530

    replace with next character

--- five.js
+++ five.js
@@ -1,16 +1,7 @@
-// Check from three given numbers that two or all of them have the same rightmost digit
+// Replace every character in a given string with the character following it in the alphabet
 
-let arr=[77,57,9747];
-function abclastdigit(arr){
-   
-    let a=arr[0]%10,b =arr[1]%10,c=arr[2]%10;
-    console.log(a,b,c);
-         if(a==b && b==c){
-            console.log(true)
-         }else{
-            console.log(false);
-         }
+let str ="JAVASCRIPT";
+
+for(let i=0;i<str.length;i++){
+console.log(String.fromCharCode(1+str[i].charCodeAt()))
 }
-abclastdigit(arr);
-
-
